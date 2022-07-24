091bcbb2611f0fb6df6a3b7be99ce78508c94675
Юлия Конышева <jukonysheva@gmail.com>
2022-07-24 13:44:29 +0300

    added files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 20000; //стоимость билета
+        int bonus = 20;
+
+        int percent = ticket / bonus;
+
+        System.out.println("Итоговый бонус:" + bonus);
+    }
+    }
